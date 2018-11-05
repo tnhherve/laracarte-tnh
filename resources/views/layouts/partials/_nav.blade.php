f1e4446bfec457087b5f518a6d1d30908d751107
Herve <tnhherve@gmail.com>
2018-11-06 00:18:54 +0100

    add set_route_active()

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -43,7 +43,7 @@
      <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
       <h5 class="my-0 mr-md-auto font-weight-normal"><a href="{{ route('home') }}">Laracarte</a> </h5>
       <nav class="my-2 my-md-0 mr-md-3 navbar-left">
-        <a class="p-2 text-dark" href="{{ route('home') }}">Home</a>
+        <a class="p-2 text-dark active" href="{{ route('home') }}">Home</a>
         <a class="p-2 text-dark" href="{{ route('about') }}">About</a>
         <a class="p-2 text-dark" href="#">Artisans</a>
         <a class="p-2 text-dark dropdown-toggle" href="https://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Planet</a>
