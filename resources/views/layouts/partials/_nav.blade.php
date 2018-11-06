a89b1a13572015b53e7c37defe8c9b33b8e36455
Herve <tnhherve@gmail.com>
2018-11-06 17:44:27 +0100

    config('app.name')

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -41,7 +41,7 @@
     </nav> -->
 
      <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 class="my-0 mr-md-auto font-weight-normal"><a href="{{ route('home') }}">Laracarte</a> </h5>
+      <h5 class="my-0 mr-md-auto font-weight-normal"><a href="{{ route('home') }}">{{config('app.name')}}</a> </h5>
       <nav class="my-2 my-md-0 mr-md-3 navbar-left">
         <a class="p-2 text-dark active" href="{{ route('home') }}">Home</a>
         <a class="p-2 text-dark" href="{{ route('about') }}">About</a>
